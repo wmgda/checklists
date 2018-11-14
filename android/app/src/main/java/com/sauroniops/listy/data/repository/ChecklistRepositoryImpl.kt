36d608184e7c486c0fef8cc98690bc4673f727b8
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2018-11-14 12:32:40 +0100

    Return mocked details.

--- android/app/src/main/java/com/sauroniops/listy/data/repository/ChecklistRepositoryImpl.kt
+++ android/app/src/main/java/com/sauroniops/listy/data/repository/ChecklistRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.algolia.search.saas.Query
 import com.google.gson.Gson
 import com.sauroniops.listy.BuildConfig
 import com.sauroniops.listy.data.model.Checklist
+import com.sauroniops.listy.data.model.ChecklistItem
 import com.sauroniops.listy.data.model.SearchResponse
 import io.reactivex.Single
 
@@ -14,6 +15,20 @@ class ChecklistRepositoryImpl(
 ) : ChecklistRepository {
 
     private val gson = Gson()
+
+    override fun get(id: String): Single<Checklist> {
+        return Single.just(
+            Checklist(
+                "one",
+                "This is first item",
+                listOf(
+                    ChecklistItem("id-1", "Todo 1", false),
+                    ChecklistItem("id-2", "Todo 2", false),
+                    ChecklistItem("id-3", "Todo 3", false)
+                )
+            )
+        )
+    }
 
     override fun search(query: String): Single<List<Checklist>> {
         return Single.create { s ->

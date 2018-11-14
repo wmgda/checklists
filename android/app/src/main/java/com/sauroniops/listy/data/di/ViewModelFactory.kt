00e8c7e2076689e61972724c89b10d5029aba809
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2018-11-14 12:55:22 +0100

    Added ViewModel for MainActivity.

--- android/app/src/main/java/com/sauroniops/listy/data/di/ViewModelFactory.kt
+++ android/app/src/main/java/com/sauroniops/listy/data/di/ViewModelFactory.kt
@@ -1,2 +1,14 @@
 package com.sauroniops.listy.data.di
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.kodein.di.DKodein
+import org.kodein.di.generic.instance
+
+class ViewModelFactory(private val injector: DKodein) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return injector.instance<ViewModel>(tag = modelClass.name) as T
+    }
+}
--- android/app/src/main/java/com/sauroniops/listy/presentation/main/MainActivity.kt
+++ android/app/src/main/java/com/sauroniops/listy/presentation/main/MainActivity.kt
@@ -4,57 +4,52 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sauroniops.listy.R
 import com.sauroniops.listy.data.model.Checklist
-import com.sauroniops.listy.data.repository.ChecklistRepository
-import com.sauroniops.listy.presentation.addTo
 import com.sauroniops.listy.presentation.item.ItemActivity
 import com.sauroniops.listy.presentation.main.adapter.MainListAdapter
-import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
 import org.kodein.di.generic.instance
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity(), KodeinAware, MainListAdapter.OnItemClickListener {
 
     override val kodein: Kodein by closestKodein()
-    private val checklistRepository: ChecklistRepository by instance()
-    private val subscriptions = CompositeDisposable()
+    private val viewModeFactory: ViewModelProvider.Factory by instance()
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this, viewModeFactory).get(MainViewModel::class.java)
+    }
+
     private lateinit var adapter: MainListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = MainListAdapter(emptyList(), this)
+        recyclerView.layoutManager = LinearLayoutManager(baseContext)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, LinearLayoutManager.VERTICAL))
+
+        registerObservers()
+    }
+
+    private fun registerObservers() {
+        viewModel.results.observe(this, Observer { adapter.fillAdapter(it) })
+    }
 
     override fun onItemClick(item: Checklist) {
         val intent = Intent(this, ItemActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, item.id)
         }
         startActivity(intent)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        adapter = MainListAdapter(emptyList(),this)
-        recyclerView.layoutManager = LinearLayoutManager(baseContext)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = adapter
-        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, LinearLayoutManager.VERTICAL))
-
-
-
-        checklistRepository.search("").subscribe({ items ->
-            adapter.fillAdapter(items)
-        }, { err ->
-            Timber.tag("kitek").d("Error during fetching: $err ")
-        }).addTo(subscriptions)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        subscriptions.clear()
     }
 }

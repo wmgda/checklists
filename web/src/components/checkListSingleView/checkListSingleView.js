bc8c3cf85c56d3b479296b58eaeb0f9c268d5607
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@outlook.com>
2018-11-14 15:25:50 +0100

    wip

--- web/src/components/checkListSingleView/checkListSingleView.js
+++ web/src/components/checkListSingleView/checkListSingleView.js
@@ -1,18 +1,68 @@
 import React, {Component} from 'react';
 import {Button, Heading} from 'react-bulma-components';
+import CheckListNew from '../checkListNew/checkListNew';
 
 export default class CheckListSingleView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: props.item,
+    };
+    console.log(this.state);
+  }
+
+  handleChange = e => {
+    this.setState({form: {
+        ...this.state.form,
+        title: e.target.value
+      }})
+  };
+
   render() {
     const { item } = this.props;
-    console.log(this.props);
+    const items = () => {
+      const items = this.state.form.items.map((el, index) => (
+        <div key={index} style={{display: 'flex', flexDirection: 'column'}}>
+          <label>{index + 1}</label>
+          <input type="text" value={el.title} onChange={(e) => {
+            const mapped = this.state.form.items.map((el, elIndex) => {
+              if (index === elIndex) {
+                return {title: e.target.value};
+              }
+              return el;
+            });
+            this.setState({form: {
+                ...this.state.form,
+                items: mapped,
+              }})
+          }}/>
+        </div>
+      ));
+      return items;
+    };
     return (
       <div>
-        <Heading>{item.title}</Heading>
         <Button
           color={'info'}
-          onClick={() => this.props.onPress()}
+          onClick={() => this.props.return()}
         >
           GetBack
+        </Button>
+        <Heading>{item.title}</Heading>
+        <form style={{margin: '10px', width: '30%', display: 'flex', flexDirection: 'column'}}>
+          <div style={{flexDirection: 'column', display: 'flex'}} >
+            <label htmlFor="title">Title</label>
+            <input type="text" onChange={this.handleChange} value={this.state.form.title}/>
+          </div>
+          <Heading> Items </Heading>
+          {items()}
+        </form>
+        <Button
+          color={'success'}
+          onClick={() => this.props.edit(this.state.form)}
+        >
+          Save
         </Button>
       </div>
     );
--- web/src/components/checklist-item/checklistItem.js
+++ web/src/components/checklist-item/checklistItem.js
@@ -2,20 +2,12 @@ import React from 'react';
 import {Box, Content, Media} from 'react-bulma-components';
 import {style} from './style';
 
-const CheckListItem = ({item, onPress}) => {
+const CheckListItem = ({item}) => {
     return (
-      <div style={style} onClick={() => onPress(item.id)}>
-        <Box>
-          <Media>
-            <Media.Item>
-              <Content>
-                <p>
-                  <strong>{item.id}</strong> <small>{item.title}</small> <small>{item.items.length}</small>
-                </p>
-              </Content>
-            </Media.Item>
-          </Media>
-        </Box>
+      <div style={style}>
+        <p>
+          <small>{item.title}</small>
+        </p>
       </div>
     );
 };

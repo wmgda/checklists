8ff5f93fa653b78a0f7c7a42c4382e7f5526ee7c
Wojciech Możejko <wojciech.mozejko@schibsted.com>
2018-11-14 13:47:03 +0100

    Add Vue frontend

---
+++ vue2/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
+import InstantSearch from 'vue-instantsearch'
+Vue.use(InstantSearch)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
